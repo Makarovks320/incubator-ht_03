f672217193282d657f7cf847bf1012fcb8f8f9e7
Makarovks320 <kirill.makarov@tafs.pro>
2022-11-21 01:10:02 +0300

    commit

--- __tests__/e2e/api.test.ts
+++ __tests__/e2e/api.test.ts
@@ -5,9 +5,33 @@ describe('/blogs', () => {
     beforeAll(async () => {
         await request(app).delete('/testing/all-data');
     });
+
     it('should return an empty array', async () => {
          await request(app)
              .get('/blogs')
              .expect(200,[]);
+    });
+
+    let createdBlog = null;
+    it('should create new blog', async () => {
+        const response = await request(app)
+            .post('/blogs')
+            .send({
+                name: "name test",
+                description: "description test",
+                websiteUrl: "websiteUrl test"
+            })
+            .expect(201);
+        createdBlog = response.body;
+        expect(createdBlog).toEqual({
+            id: expect.any(String),
+            name: "name test",
+            description: "description test",
+            websiteUrl: "websiteUrl test"
+        });
+
+        await request(app)
+            .get('/blogs')
+            .expect(200,[createdBlog]);
     })
 });
