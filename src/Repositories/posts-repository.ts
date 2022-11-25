6b4ebb6083ab24cb004c638854dd25a95b803f43
Makarovks320 <kirill.makarov@tafs.pro>
2022-11-26 00:15:15 +0300

    duplicate post router & repo

--- src/Repositories/posts-repository.ts
+++ src/Repositories/posts-repository.ts
@@ -1,10 +1,10 @@
 type post = {
-    id: string,
-    title: string,
-    shortDescription: string,
-    content: string,
-    blogId: string,
-    blogName: string
+    id?: string,
+    title?: string,
+    shortDescription?: string,
+    content?: string,
+    blogId?: string,
+    blogName?: string
 }
 let posts: post[] = [
     {
@@ -29,11 +29,45 @@ export const postsRepository = {
     getAllPosts(): post[]{
         return posts;
     },
-    findPostById(id: string): post{
+    findPostById(id: string): post | null{
         const post = posts.filter(b => b.id === id);
-        return post[0];
+        return post ? post[0] : null;
     },
     deleteAllPosts(): void {
         posts = [];
+    },
+    createNewPost(p: post): post {
+        const newPost = {
+            id: (new Date().valueOf()).toString(),
+            title: p.title || 'mock',
+            shortDescription: p.shortDescription || 'mock',
+            content: p.content || 'mock',
+            blogId: p.blogId || 'mock',
+            blogName: p.blogName || 'mock',
+
+        };
+        posts.push(newPost);
+        return newPost;
+    },
+    updatePostById(id: string, p: post): post | null {
+        const postToUpdate = this.findPostById(id);
+        if (!postToUpdate) {
+            return null;
+        }
+        updatePost(postToUpdate, p);
+        return postToUpdate;
+    },
+    deletePostById(id: string): null | void {
+        const postToDelete = this.findPostById(id);
+        if (!postToDelete) return null;
+        posts = posts.filter(b => b.id !== id);
     }
 };
+
+function updatePost(post: post, newData: post): void {
+    newData.title ? post.title = newData.title : null;
+    newData.shortDescription ? post.shortDescription = newData.shortDescription : null;
+    newData.content ? post.content = newData.content : null;
+    newData.blogId ? post.blogId = newData.blogId : null;
+    newData.blogName ? post.blogName = newData.blogName : null;
+}
--- src/Routers/postsRouter.ts
+++ src/Routers/postsRouter.ts
@@ -1,5 +1,7 @@
 import {Request, Response, Router} from "express";
+import {body} from "express-validator";
 import {postsRepository} from "../Repositories/posts-repository";
+import {inputValidator} from "../middlewares/inputValidator";
 
 
 export const postsRouter = Router();
@@ -15,6 +17,33 @@ postsRouter.delete('/', ((req: Request, res: Response) => {
 }));
 
 postsRouter.get('/:id', ((req: Request, res: Response) => {
-    const blog = postsRepository.findPostById(req.params.id);
-    res.send(blog);
+    const post = postsRepository.findPostById(req.params.id);
+    res.send(post);
 }));
+
+postsRouter.post('/',
+    body('title').isString().withMessage('should be string'),
+    body('shortDescription').isString().withMessage('should be string'),
+    body('content').isString().withMessage('should be string'),
+    body('blogId').isString().withMessage('should be string'),
+    body('blogName').isString().withMessage('should be string'),
+    inputValidator,
+    ((req: Request, res: Response) => {
+        const post = req.body;
+        const newPost = postsRepository.createNewPost(post);
+        res.status(201).send(newPost);
+    }));
+
+postsRouter.put('/:id',
+    body('title').isString().withMessage('should be string'),
+    inputValidator,
+    (req: Request, res: Response) => {
+        const updatedPost = postsRepository.updatePostById(req.params.id, req.body);
+        updatedPost ? res.status(204).send() : res.status(404).send();
+    });
+
+postsRouter.delete('/:id',
+    ((req: Request, res: Response) => {
+        const post = postsRepository.deletePostById(req.params.id);
+        post ? res.status(204).send() : res.status(404).send();
+    }));
