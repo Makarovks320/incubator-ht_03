ca68c54ae60113d2e5a719bf2fa50d462f36ceb8
Makarovks320 <kirill.makarov@tafs.pro>
2022-11-26 02:32:42 +0300

    authorization

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
-export function authorization (req: Request, res: Response, next: NextFunction) {
+export const authorization = (req: Request, res: Response, next: NextFunction) => {
     if (!req.headers.authorization) res.status(401).send();
     req.headers.authorization === 'Basic YWRtaW46cXdlcnR5' ? next()
         : res.status(401).send();
-}
+};
