06bf38033e21d322d514762d865ff1328ccd28cc
Makarovks320 <kirill.makarov@tafs.pro>
2022-11-27 21:59:59 +0300

    fix: check id from URI

--- src/Routers/postsRouter.ts
+++ src/Routers/postsRouter.ts
@@ -4,6 +4,7 @@ import {postsRepository} from "../Repositories/posts-repository";
 import {inputValidator} from "../middlewares/inputValidator";
 import {authorization} from "../middlewares/authorization";
 import {checkBlogIdExists} from "../middlewares/checkBlogIdExists";
+import {checkIdFromUri} from "../middlewares/checkIdFromUri";
 
 
 export const postsRouter = Router();
@@ -41,10 +42,11 @@ postsRouter.post('/',
 
 postsRouter.put('/:id',
     authorization,
-    body('title').trim().isLength({max: 30}).withMessage('should be string').not().isEmpty(),
-    body('shortDescription').trim().isLength({max: 100}).withMessage('should be string').not().isEmpty(),
-    body('content').trim().isLength({max: 1000}).withMessage('should be string').not().isEmpty(),
-    body('blogId').trim().isString().custom(checkBlogIdExists).withMessage('should be string'),
+    checkIdFromUri,
+    body('title').trim().isLength({max: 30}).withMessage('max: 30').not().isEmpty(),
+    body('shortDescription').trim().isLength({max: 100}).withMessage('max: 100').not().isEmpty(),
+    body('content').trim().isLength({max: 1000}).withMessage('max: 1000').not().isEmpty(),
+    body('blogId').trim().isString().custom(checkBlogIdExists).withMessage('blog Id not found'),
     inputValidator,
     (req: Request, res: Response) => {
         const updatedPost = postsRepository.updatePostById(req.params.id, req.body);
