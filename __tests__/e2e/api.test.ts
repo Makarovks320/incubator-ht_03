b51c6e5b6992a29b543fe6cf46be59280fd64aa7
Makarovks320 <kirill.makarov@tafs.pro>
2022-12-21 01:32:38 +0300

    e2e test update

--- __tests__/e2e/api.test.ts
+++ __tests__/e2e/api.test.ts
@@ -13,22 +13,36 @@ describe('/blogs', () => {
              .expect(200,[]);
     });
 
+    it('should not return error (websiteUrl)', async () => {
+        await request(app)
+            .post('/blogs')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .send({
+                name: "name test",
+                description: "description test",
+                websiteUrl: "wrong"
+            })
+            .expect(400,{ errorsMessages: [ { message: 'Invalid value', field: 'websiteUrl' } ] });
+    });
+
     let createdBlog: blog | null = null;
     it('should create new blog', async () => {
         const response = await request(app)
             .post('/blogs')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
             .send({
                 name: "name test",
                 description: "description test",
-                websiteUrl: "websiteUrl test"
+                websiteUrl: "http://test.ru"
             })
             .expect(201);
         createdBlog = response.body;
         expect(createdBlog).toEqual({
+            createdAt: expect.any(String),
             id: expect.any(String),
             name: "name test",
             description: "description test",
-            websiteUrl: "websiteUrl test"
+            websiteUrl: "http://test.ru"
         });
 
         await request(app)
@@ -42,10 +56,9 @@ describe('/blogs', () => {
             .expect(200,createdBlog);
     });
 
-
     it('should return 404', async () => {
         await request(app)
-            .get('/blogs/' + 5)
+            .get('/blogs/' + 'wrong-id-number')
             .expect(404);
     });
 });
