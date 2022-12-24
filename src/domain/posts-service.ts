5df92180a91bdc8d681e84659030d380455e6d3e
Makarovks320 <kirill.makarov@tafs.pro>
2022-12-25 01:38:41 +0300

    создал postService

---
+++ src/domain/posts-service.ts
@@ -0,0 +1,41 @@
+import {postsRepository} from "../Repositories/posts-repository";
+
+type post = {
+    id?: string,
+    title?: string,
+    shortDescription?: string,
+    content?: string,
+    blogId?: string,
+    blogName?: string,
+    createdAt?: string
+}
+
+export const postsService = {
+    async getAllPosts(): Promise<post[]>{
+        return postsRepository.getAllPosts();
+    },
+    async findPostById(id: string): Promise<post | null>{
+        return postsRepository.findPostById(id);
+    },
+    async deleteAllPosts(): Promise<void> {
+        await postsRepository.deleteAllPosts();
+    },
+    async createNewPost(p: post): Promise<post> {
+        const newPost: post = {
+            id: (new Date().valueOf()).toString(),
+            title: p.title || 'mock',
+            shortDescription: p.shortDescription || 'mock',
+            content: p.content || 'mock',
+            blogId: p.blogId || 'mock',
+            blogName: p.blogName || 'mock',
+            createdAt: (new Date()).toISOString()
+        };
+        return await postsRepository.createNewPost(newPost);
+    },
+    async updatePostById(id: string, p: post): Promise<boolean> {
+        return postsRepository.updatePostById(id, p);
+    },
+    async deletePostById(id: string): Promise<boolean> {
+        return postsRepository.deletePostById(id);
+    }
+};
